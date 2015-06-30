9019dfd8b429b246eb3b496abcb9a5ad5099b532
Shaun Harker <sharker81@gmail.com>
2015-06-30 03:00:29 -0400

    improving testing

--- tests/TestDomain.cpp
+++ tests/TestDomain.cpp
@@ -15,15 +15,16 @@ int main ( int argc, char * argv [] ) {
     if ( (d1 ++)[0] != 1 ) throw std::runtime_error ( "Domain postincrement bug" );
     if ( d1 . size () != 3 ) throw std::runtime_error ( "Domain::size bug");
     if ( d1 . index () != 2 ) throw std::runtime_error ( "Domain::index bug");
-    if ( d1 . right ( 1 ) != 27 ) throw std::runtime_error ( "Domain::right bug");
+    if ( d1 . right ( 1 ) != 7 ) throw std::runtime_error ( "Domain::right bug");
     if ( d1 . left ( 0 ) != 1 ) throw std::runtime_error ( "Domain::left bug");
     if ( not d1 . isMin ( 2 ) ) throw std::runtime_error ( "Domain::isMin bug");
-    d1 . setIndex ( 5 );
+    d1 . setIndex ( 4 );
     if ( not d1 . isMax ( 0 ) ) throw std::runtime_error ( "Domain::isMax bug");
     std::cout << d1;
     boost::archive::text_oarchive oa(std::cout);
     oa << d1;
-  } catch ( ... ) {
+  } catch ( std::exception & e ) {
+    std::cout << e . what () << "\n";
     return 1;
   }
   return 0;
--- tests/unittests.sh
+++ tests/unittests.sh
@@ -1,47 +1,16 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
-./TestAnnotation.x > /dev/null
-./TestWall.x > /dev/null
-./TestDomain.x > /dev/null
-./TestDomainGraph.x > /dev/null
-./TestMorseGraph.x > /dev/null
-./TestOrderParameter.x > /dev/null
-./TestMorseDecomposition.x > /dev/null
-./TestNetwork.x > /dev/null
-./TestParameterGraph.x > /dev/null
-dsgrn > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt > /dev/null
-dsgrn network draw > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt domaingraph graphviz '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt wallgraph graphviz '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt morsedecomposition graphviz '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt morsegraph graphviz '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt domaingraph json '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt wallgraph json '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt morsedecomposition json '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt morsegraph json '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt domaingraph graphviz 234 > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt wallgraph graphviz 234 > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt morsedecomposition graphviz 234 > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt morsegraph graphviz 234 > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt domaingraph json 234 > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt wallgraph json 234 > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt morsedecomposition json 234 > /dev/null
-dsgrn network ../networks/3D_Cycle_H.txt morsegraph json 234 > /dev/null
-dsgrn parameter > /dev/null
-dsgrn parameter list > /dev/null
-dsgrn parameter json '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn parameter json 234 > /dev/null
-dsgrn parameter inequalities '[["X1",[3,3,"FF9E00"],[0,1,2]],["X2",[1,2,"4"],[0,1]],["X3",[2,1,"8"],[0]]]' > /dev/null
-dsgrn parameter inequalities 234 > /dev/null
-dsgrn analyze morseset 0 0 > /dev/null
-
-# Tests meant to fail
-dsgrn fail > /dev/null || echo success > /dev/null
-dsgrn parameter inequalities > /dev/null || echo success > /dev/null
-#dsgrn parameter index 0 > /dev/null || echo success > /dev/null
-#dsgrn domaingraph > /dev/null || echo success > /dev/null
-#dsgrn wallgraph > /dev/null || echo success > /dev/null
-#dsgrn morsedecomposition > /dev/null || echo success > /dev/null
-#dsgrn morsegraph > /dev/null || echo success > /dev/null
+var=0
+while read p; do
+  eval $p > /dev/null 
+  if [ ! $? -eq 0 ]; then
+    echo FAIL: $p
+    var=$((var+1))
+  fi
+done < unittests.txt
+if [[ "$var" -eq "0" ]]; then
+  exit 0
+else
+  exit 1
+fi
