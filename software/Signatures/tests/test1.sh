3bcd9ed97e4e01795e904c9547f8e985935c17d0
Shaun Harker <sharker81@gmail.com>
2015-07-01 16:13:32 -0400

    fixing test

--- software/Signatures/tests/test1.sh
+++ software/Signatures/tests/test1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SRC_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 rm -f $SRC_ROOT/test.db
-mpiexec -np 2 $SRC_ROOT/../bin/Signatures $SRC_ROOT/../../../networks/2D_Example.txt $SRC_ROOT/test.db || exit 1
+mpiexec -np 2 $SRC_ROOT/../build/bin/Signatures $SRC_ROOT/../../../networks/2D_Example.txt $SRC_ROOT/test.db || exit 1
 X=`sqlite3 $SRC_ROOT/test.db 'select count(*) from MorseGraphViz;'` || exit 1
 if [[ "$X" -eq "8" ]]; then
   exit 0
