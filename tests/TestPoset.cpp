03399f5512e322f6254e04b0437d046caa69f9be
Arnaud Goullet <arnaud.goullet@gmail.com>
2016-01-13 14:13:08 -0500

    adding test for poset

--- tests/TestPoset.cpp
+++ tests/TestPoset.cpp
@@ -15,6 +15,9 @@ int main ( int argc, char * argv [] ) {
     uint64_t n2 = ps . add_vertex ();
     uint64_t n3 = ps . add_vertex ();
     uint64_t n4 = ps . add_vertex ();
+    uint64_t n5 = ps . add_vertex ();
+    uint64_t n6 = ps . add_vertex ();
+
     ps . add_edge ( n0, n1 );
     ps . add_edge ( n0, n2 );
     ps . add_edge ( n1, n3 );
@@ -22,12 +25,25 @@ int main ( int argc, char * argv [] ) {
     ps . add_edge ( n3, n4 );
 
     ps . add_edge ( n0, n3 );
+    ps . add_edge ( n4, n5 );
+    ps . add_edge ( n4, n6 );
+
+    std::cout << ps;
 
     ps . reduction ( );
+
+    for ( uint64_t i=0; i<ps.size(); ++i ) {
+      std::cout << "Node " << i << "\n";
+      std::cout << "#ancestors   : " << ps.numberOfAncestors(i) << "\n";
+      std::cout << "#descendants : " << ps.numberOfDescendants(i) << "\n";
+      std::cout << "#parents     : " << ps.numberOfParents(i) << "\n";
+      std::cout << "#children    : " << ps.numberOfChildren(i) << "\n";
+    }
 
     std::cout << ps;
     boost::archive::text_oarchive oa(std::cout);
     oa << ps;
+
 
   } catch ( std::exception & e ) {
     std::cout << e . what () << "\n";
