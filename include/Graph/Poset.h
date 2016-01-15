cb409fd497be0d290ed45d76a1c2c12a6235c08b
Arnaud Goullet <arnaud.goullet@gmail.com>
2016-01-14 22:55:02 -0500

    [WIP] rewrite methods without the need of matrices. Added shared_pointer on struct for the poset class.

--- include/Graph/Poset.h
+++ include/Graph/Poset.h
@@ -9,41 +9,46 @@
 
 #include "Graph/Digraph.h"
 
+struct Poset_;
+
 /// class Poset
 class Poset : public Digraph {
 public:
+
   /// reduction
   ///   Perform a transitive reduction
   ///   (i.e. use Hasse diagram representation)
   void
   reduction ( void );
 
-  // Return the number of ancestors of node i
-  uint64_t numberOfAncestors( const uint64_t & i ) const;
-  // Return the number of descendants of node i
-  uint64_t numberOfDescendants( const uint64_t & i ) const;
-  // Return the number of parents of node i
-  uint64_t numberOfParents ( const uint64_t & i ) const;
-  // Return the number of children of node i
-  uint64_t numberOfChildren ( const uint64_t & i ) const;
+  /// Return the number of ancestors of node i
+  uint64_t
+  numberOfAncestors( const uint64_t & i ) const;
+  /// Return the number of descendants of node i
+  uint64_t
+  numberOfDescendants( const uint64_t & i ) const;
+  /// Return the number of parents of node i
+  uint64_t
+  numberOfParents ( const uint64_t & i ) const;
+  /// Return the number of children of node i
+  uint64_t
+  numberOfChildren ( const uint64_t & i ) const;
 
-  // Check if we have an edge n -> m with n < m
-  bool topologicallySorted ( const uint64_t & n, const uint64_t & m ) const;
+  /// Check if we have the reachability n -> m
+  /// Assume we have a topologically sorted digraph ( n < m )
+  bool reachable ( const uint64_t & n, const uint64_t & m ) const;
 
 private:
-  std::vector<std::vector<uint64_t>> reachability_;
-  std::vector<std::vector<uint64_t>> transitiveReduction_;
-
-  void sumRowReachability  ( void );
-  std::vector<uint64_t> sumRowReachability_;
-  void sumColumnReachability  ( void );
-  std::vector<uint64_t> sumColumnReachability_;
-  void sumRowTransitiveReduction  ( void );
-  std::vector<uint64_t> sumRowTransitiveReduction_;
-  void sumColumnTransitiveReduction  ( void );
-  std::vector<uint64_t> sumColumnTransitiveReduction_;
-
-  void warshall ( void );
+  std::shared_ptr<Poset_> dataPoset_;
+  //
+  void
+  computeNumberOfParents ( void) const;
+  void
+  computeNumberOfChildren ( void ) const;
+  void
+  computeNumberOfAncestors ( void ) const;
+  void
+  computeNumberOfDescendants ( void ) const;
 
   /// serialize
   ///   For use with BOOST Serialization library,
@@ -52,6 +57,28 @@ private:
   template<class Archive>
   void serialize(Archive & ar, const unsigned int version) {
     ar & boost::serialization::base_object<Digraph>(*this);
+    ar & dataPoset_;
+  }
+};
+
+struct Poset_ {
+
+  std::vector<std::vector<uint64_t>> reachability_;
+
+  std::vector<uint64_t> parentsCount_, childrenCount_;
+  std::vector<uint64_t> ancestorsCount_, descendantsCount_;
+
+  /// serialize
+  ///   For use with BOOST Serialization library,
+  ///   which is used by the cluster-delegator MPI package
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version) {
+    ar & reachability_;
+    ar & parentsCount_;
+    ar & childrenCount_;
+    ar & ancestorsCount_;
+    ar & descendantsCount_;
   }
 };
 
--- include/Graph/Poset.hpp
+++ include/Graph/Poset.hpp
@@ -19,10 +19,15 @@ reduction ( void ) {
   // (Assumes original state is transitively closed.)
   //
   uint64_t N = size ();
-  // construct the reachability matrix using warshall algorithm
-  // and also precompute some quantities
-  // 1^T A and A 1 with A the reachability matrix
-  warshall();
+
+  dataPoset_ -> parentsCount_ . resize ( N );
+  //   dataPoset_ -> childrenCount_ . resize ( N );
+  //   dataPoset_ -> ancestorsCount_ . resize ( N );
+  //   dataPoset_ -> descendantsCount_ . resize ( N );
+  //   computeNumberOfAncestors();
+  //   computeNumberOfDescendants();
+
+
   //
   for ( uint64_t u = 0; u < N; ++ u ) {
     std::unordered_set<uint64_t> double_hop;
@@ -43,111 +48,69 @@ reduction ( void ) {
     data_ -> adjacencies_[u].erase ( it, data_ ->adjacencies_[u].end() );
   }
 
-  // make a copy (necessary?)
-  transitiveReduction_ = data_ -> adjacencies_;
-  // Pre-compute some quantities
-  sumRowTransitiveReduction();
-  sumColumnTransitiveReduction();
-  //
+  computeNumberOfParents();
+  computeNumberOfChildren();
+
 }
 
 INLINE_IF_HEADER_ONLY uint64_t Poset::
 numberOfAncestors ( const uint64_t & i ) const {
-  return sumRowReachability_ [ i ];
+  return dataPoset_ -> ancestorsCount_ [ i ];
 }
 
 INLINE_IF_HEADER_ONLY uint64_t Poset::
 numberOfDescendants ( const uint64_t & i ) const {
-  return sumColumnReachability_ [ i ];
+  return dataPoset_ -> descendantsCount_ [ i ];
 }
 
 INLINE_IF_HEADER_ONLY uint64_t Poset::
 numberOfParents ( const uint64_t & i ) const {
-  return sumRowTransitiveReduction_ [ i ];
+  return dataPoset_ -> parentsCount_ [ i ];
 }
 
 INLINE_IF_HEADER_ONLY uint64_t Poset::
 numberOfChildren ( const uint64_t & i ) const {
-  return sumColumnTransitiveReduction_ [ i ];
+  return dataPoset_ -> childrenCount_ [ i ];
 }
 
-// If B is the transitive reduction matrix,
-// we compute 1^T B
 INLINE_IF_HEADER_ONLY void Poset::
-sumRowTransitiveReduction ( void ) {
-  //
-  sumRowTransitiveReduction_ . resize ( size() , 0 );
-  //
-  for ( auto u : transitiveReduction_ ) {
-    for ( auto v : u ) {
-      ++ sumRowTransitiveReduction_[v];
+computeNumberOfAncestors ( void ) const {
+  uint64_t N = size();
+  for ( uint64_t i=0; i<N; ++i ) {
+    for ( auto u : dataPoset_ -> reachability_ [ i ] ) {
+      ++ dataPoset_ -> ancestorsCount_ [ u ];
     }
   }
 }
 
-// If B is the transitive reduction matrix
-// we compute B 1, with 1 is the one vector
-// i.e., add up the columns (component wise) = list adjacency . size()
 INLINE_IF_HEADER_ONLY void Poset::
-sumColumnTransitiveReduction ( void ) {
-  // Number of nodes
+computeNumberOfDescendants ( void ) const {
   uint64_t N = size();
-  //
-  sumColumnTransitiveReduction_ . resize ( N, 0 );
-  //
   for ( uint64_t i=0; i<N; ++i ) {
-    sumColumnTransitiveReduction_ [ i ] = transitiveReduction_ [ i ] . size();
+    dataPoset_ -> descendantsCount_ [ i ] =  dataPoset_ -> reachability_ [ i ] . size();
   }
 }
 
 INLINE_IF_HEADER_ONLY void Poset::
-warshall ( void ) {
+computeNumberOfParents ( void ) const {
   uint64_t N = size();
-  //
-  bool A[N][N];
-  //
   for ( uint64_t i=0; i<N; ++i ) {
-    for ( uint64_t j=0; j<N; ++j ) {
-      A[i][j] = false;
-    }
-  }
-  // Construct the adjacency matrix
-  for ( uint64_t i=0; i<data_->adjacencies_.size(); ++i ) {
     for ( auto u : data_ -> adjacencies_ [ i ] ) {
-      A [ i ] [ u ] = true;
-    }
-  }
-  // Algorithm
-  for ( uint64_t k=0; k<N; ++k ) {
-    for ( uint64_t i=0; i<N; ++i ) {
-      for ( uint64_t j=0; j<N; ++j ) {
-        A [ i ] [ j ] = A[i][j] || (A[i][k] && A[k][j]);
-      }
-    }
-  }
-  // Fill the reachability matrix
-  // and precompute some quantities
-  reachability_ . resize ( N );
-  sumRowReachability_ . resize ( N, 0 );
-  sumColumnReachability_ . resize ( N, 0 );
-  for ( uint64_t i=0; i<N; ++i ) {
-    reachability_ [ i ] . resize ( N, 0 );
-  }
-  for ( uint64_t i=0; i<N; ++i ) {
-    // need to go through half of the matrix
-    for ( uint64_t j=i; j<N; ++j ) {
-      if ( A[i][j] ) {
-        reachability_[i][j] = 1;
-        ++sumRowReachability_[j]; // Sum of the rows component wise : 1^T A
-        ++sumColumnReachability_[i]; // Sum of the columns component wise : A 1
-      }
+      ++ dataPoset_ -> parentsCount_ [ u ];
     }
   }
 }
 
+INLINE_IF_HEADER_ONLY void Poset::
+computeNumberOfChildren ( void ) const {
+  uint64_t N = size();
+  for ( uint64_t i=0; i<N; ++i ) {
+    dataPoset_ -> childrenCount_ [ i ] =  data_ -> adjacencies_ [ i ] . size();
+  }
+}
 
 INLINE_IF_HEADER_ONLY bool Poset::
-topologicallySorted ( const uint64_t & n, const uint64_t & m ) const {
+reachable ( const uint64_t & n, const uint64_t & m ) const {
   for ( auto u : data_ -> adjacencies_ [ n ] ) {
     if ( u == m ) {
       return true;
