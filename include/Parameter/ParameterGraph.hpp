d4a5d26b200786ebfada7bdf1039d7aea14a9a53
Arnaud Goullet <arnaud.goullet@gmail.com>
2016-02-18 12:36:01 -0500

    [WIP] added some comments

--- include/Parameter/ParameterGraph.hpp
+++ include/Parameter/ParameterGraph.hpp
@@ -12,14 +12,14 @@
 #include "ParameterGraph.h"
 
 INLINE_IF_HEADER_ONLY ParameterGraph::
-ParameterGraph ( void ) { 
+ParameterGraph ( void ) {
   data_ . reset ( new ParameterGraph_ );
 }
 
 INLINE_IF_HEADER_ONLY ParameterGraph::
 ParameterGraph ( Network const& network, std::string const& path ) {
   assign ( network, path );
-} 
+}
 
 INLINE_IF_HEADER_ONLY void ParameterGraph::
 assign ( Network const& network, std::string const& path ) {
@@ -43,7 +43,7 @@ assign ( Network const& network, std::string const& path ) {
     //std::cout << "Acquiring logic data in " << ss.str() << "\n";
     std::vector<std::string> hex_codes;
     std::ifstream infile ( ss.str() );
-    if ( not infile . good () ) { 
+    if ( not infile . good () ) {
       throw std::runtime_error ( "Error: Could not find logic resource " + ss.str() + ".\n");
     }
     std::string line;
@@ -65,7 +65,7 @@ size ( void ) const {
 INLINE_IF_HEADER_ONLY Parameter ParameterGraph::
 parameter ( uint64_t index ) const {
   //std::cout << data_ -> "ParameterGraph::parameter( " << index << " )\n";
-  if ( index >= size () ) { 
+  if ( index >= size () ) {
     throw std::runtime_error ( "ParameterGraph::parameter Index out of bounds");
   }
   uint64_t logic_index = index % data_ -> fixedordersize_;
@@ -104,15 +104,17 @@ parameter ( uint64_t index ) const {
 INLINE_IF_HEADER_ONLY uint64_t ParameterGraph::
 index ( Parameter const& p ) const {
 //   throw std::runtime_error ( "Feature not implemented" );  // TODO
-  
+
   std::vector<LogicParameter> logic = p . logic ( );
   std::vector<OrderParameter> order = p . order ( );
-  
+
   /// Construct Logic indices
   std::vector<uint64_t> logic_indices;
   uint64_t D = data_ -> network_ . size ();
   for ( uint64_t d = 0; d< D; ++d ) {
-      std::string hexcode = logic [ d ] . hex ( );    
+      std::string hexcode = logic [ d ] . hex ( );
+      /// If factors_ was a different container, we could use find instead
+      /// find the hex code within factors_ for the appropriate node
       uint64_t hexsize = data_ -> factors_ [ d ] . size ( );
       for ( uint64_t i = 0; i < hexsize; ++i ) {
           if ( hexcode == data_ -> factors_ [ d ] [ i ] ) {
@@ -120,20 +122,24 @@ index ( Parameter const& p ) const {
           }
       }
   }
-  std::cout << "\nLogic indices:\n"; 
+  // debug
+  std::cout << "\nLogic indices:\n";
   for ( uint64_t d = 0; d< D; ++d ) std::cout << logic_indices[d] << " ";
   std::cout << "\n";
-  
-  /// Order indices 
+  // end debug
+
+  /// Construct Order indices
   std::vector<uint64_t> order_indices;
   for ( uint64_t d = 0; d < D; ++ d ) {
       order_indices . push_back ( order[d].index() );
   }
-  std::cout << "\nOrder indices:\n"; 
+  // debug
+  std::cout << "\nOrder indices:\n";
   for ( uint64_t d = 0; d< D; ++d ) std::cout << order_indices[d] << " ";
   std::cout << "\n";
+  // end debug
 
-
+  // Debug
   std::cout << "\n\n";
   std::cout << "Logic_place_values : ";
   for ( uint64_t d = 0; d< D; ++d ) std::cout << data_ -> logic_place_values_[d] << " ";
@@ -143,27 +149,26 @@ index ( Parameter const& p ) const {
   std::cout << "\n";
   std::cout << "Fixed order size : " << data_ -> fixedordersize_;
   std::cout << "\n\n";
-
+  // end debug
 
   /// Return : m * Q + R
-  auto getInteger = []( const uint64_t &m, const uint64_t &Q, const uint64_t &R ) -> uint64_t { 
-      return m * Q + R; 
+  auto getInteger = []( const uint64_t &m, const uint64_t &Q, const uint64_t &R ) -> uint64_t {
+      return m * Q + R;
   };
 
+  typedef std::vector<uint64_t> Interval;
   /// Return interval of the form : [ m*n*k+m*a, m*n*k+m*a+m )
   auto getInterval = [](uint64_t &k, uint64_t &m, uint64_t &n, uint64_t &a ) -> std::vector<uint64_t>  {
       return std::vector<uint64_t> { m*n*k+m*a,m*n*k+m*a+m };
   };
 
-  typedef std::vector<uint64_t> Interval;
-
   auto insideInterval = [](uint64_t &n, Interval &interval) -> bool {
-    return n>=interval[0] && n<=interval[1];  
+    return n>=interval[0] && n<=interval[1];
   };
 
-  // List all the subintervals for logic_index and store the potential good values for logic index 
+  // List all the subintervals for logic_index and store the potential good values for logic index
   std::cout << "\n\nList of all subintervals for logic index\n";
-  std::vector<Interval> intervals;
+  std::vector<Interval> intervals; // needed just for debug 
   std::vector<uint64_t> logicindexvalues;
   for ( uint64_t R = 0; R<data_->reorderings_; ++R ) {
       Interval interval = getInterval( R,
@@ -172,7 +177,7 @@ index ( Parameter const& p ) const {
                                        logic_indices[1] );
       std::cout << "[ " << interval[0] << ", " << interval[1] << " ]\n";
       intervals . push_back ( interval );
-      /// check for good values inside the interval 
+      /// check for good values inside the interval
       uint64_t myvalue = getInteger(data_ -> logic_place_values_[0],
                                     R*data_ -> logic_place_values_[1]+logic_indices[1],
                                     logic_indices[0]);
@@ -180,18 +185,17 @@ index ( Parameter const& p ) const {
       if ( insideInterval(myvalue,interval) ) {
           std::cout << "is good\n";
           logicindexvalues . push_back ( myvalue );
-      } else { 
+      } else {
           std::cout << "is bad\n";
-      }  
+      }
   }
-  // List all the subintervals for order_index and store the potential good values for order index 
-  std::vector<uint64_t> orderindexvalues;
-  
-  
-  
+  // List all the subintervals for order_index and store the potential good values for order index
+
+
+
 
   return 0;
- 
+
 }
 
 INLINE_IF_HEADER_ONLY std::vector<uint64_t> ParameterGraph::
@@ -205,7 +209,7 @@ network ( void ) const {
 }
 
 INLINE_IF_HEADER_ONLY std::ostream& operator << ( std::ostream& stream, ParameterGraph const& pg ) {
-  stream <<  "(ParameterGraph: "<< pg.size() << " parameters, " 
+  stream <<  "(ParameterGraph: "<< pg.size() << " parameters, "
          << pg.network().size() << " nodes)";
   return stream;
 }
@@ -222,7 +226,7 @@ reorderings ( void ) const {
 
 INLINE_IF_HEADER_ONLY uint64_t ParameterGraph::
 _factorial ( uint64_t m ) const {
-  static const std::vector<uint64_t> table = 
+  static const std::vector<uint64_t> table =
     { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
   if ( m < 10 ) return table [ m ]; else return m * _factorial ( m - 1 );
 }
