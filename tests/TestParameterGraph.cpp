17d2f17b84ebbaf86e5304dfce011f8ed609babb
Arnaud Goullet <arnaud.goullet@gmail.com>
2016-02-21 00:02:43 -0500

    modified testParameterGraph to test the index method with a larger network

--- tests/TestParameterGraph.cpp
+++ tests/TestParameterGraph.cpp
@@ -9,37 +9,44 @@ int main ( int argc, char * argv [] ) {
   try {
     ParameterGraph pg0;
     std::string filename;
-    if ( argc < 2 ) filename = "networks/network2.txt";
+    if ( argc < 2 ) filename = "networks/network3.txt";
     else filename = argv[1];
     Network network ( filename );
     ParameterGraph pg ( network );
     std::cout << pg << "\n";
     std::cout << pg . network () << "\n";
     uint64_t N = pg . size ();
-    for ( uint64_t i = 0; i < N; ++ i ) {
-      Parameter p = pg . parameter ( i );
-      Parameter q = pg . parameter ( 0 );
-      std::string s = p . stringify ();
-      q . parse ( s );
-      std::cout << q << "\n";
-    }
-    if ( pg . size () != 120 ) throw std::runtime_error ( "ParameterGraph::size bug");
-    if ( pg . fixedordersize () != 60 ) throw std::runtime_error ( "ParameterGraph::fixedordersize bug");
-    if ( pg . reorderings () != 2 ) throw std::runtime_error ( "ParameterGraph::reorderings bug");
+    // for ( uint64_t i = 0; i < N; ++ i ) {
+    //   Parameter p = pg . parameter ( i );
+    //   Parameter q = pg . parameter ( 0 );
+    //   std::string s = p . stringify ();
+    //   q . parse ( s );
+      // std::cout << q << "\n";
+    // }
+    // if ( pg . size () != 120 ) throw std::runtime_error ( "ParameterGraph::size bug");
+    // if ( pg . fixedordersize () != 60 ) throw std::runtime_error ( "ParameterGraph::fixedordersize bug");
+    // if ( pg . reorderings () != 2 ) throw std::runtime_error ( "ParameterGraph::reorderings bug");
 
     // Missing Features (currently they throw):
     Parameter z = pg . parameter ( 0 );
-    try { 
+    try {
         for ( uint64_t i = 0; i < N; ++i ) {
-            std::cout << "-------\n";
-            std::cout << "The index was : " << i << "\n\n";  
-            uint64_t j = pg . index ( pg . parameter(i) );  
-            std::cout << "-------\n";
-              
+            // std::cout << "-------\n";
+            // std::cout << "The index was : " << i << "\n\n";
+            uint64_t j = pg . index ( pg . parameter(i) );
+            // std::cout << "index returned : " << j << "\n";
+            // std::cout << "-------\n";
+
+            if ( i != j ) {
+              std::cout << "Wrong index\n";
+              std::cout << "The index was " << i << " but returned " << j << "\n";
+            }
+
+
         }
-      
+
     } catch ( ... ) {}
-    try { 
+    try {
       auto adj = pg . adjacencies ( 0 );
     } catch ( ... ) {}
     try {
