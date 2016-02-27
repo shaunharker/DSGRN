93d14fd55de3e682fffd390c0826a69c4ca97b3f
Mike Lan <mikelan17@gmail.com>
2016-02-27 12:44:46 -0500

    fix main script

--- software/HillSimulations/runmany2b.sh
+++ software/HillSimulations/runmany2b.sh
@@ -13,7 +13,7 @@ resultsfile=$2
 savein_location=$3
 parameter_file_path=$4
 
-extrema_file = $savein_location'TimeSeries.txt'
+extrema_file=$savein_location'TimeSeries.txt'
 if [ -f $extrema_file ] 
   then
       rm $extrema_file
@@ -32,8 +32,8 @@ for num in "${arr[@]}"
 do
   #remove because this script writes the output after the file's old contents, instead of overwriting all its old contents
   #This is in contrast to 'file_ = open(samples, 'w')' in python
-  parameters_file = $parameter_file_path$num.txt 
-  if [ -f $parameters_file] 
+  parameters_file=$parameter_file_path$num.txt 
+  if [ -f $parameters_file ] 
   then
       rm $parameters_file
   fi
@@ -52,7 +52,7 @@ python compare.py
 #4 total inputs: 1.resultsfile, 2.networkfile, 3.save_location (folder to save extrema_location (save comparison) and savein (save FCs))
 #and 4.parameter_file_path (where to save and retrieve files from DSGRN)
 
-#example: ./runmany2b.sh '/home/mike/DSGRN-master/networks/5D_2016_01_28.txt' '/home/mike/conley3_outputs/5D_2016_01_28_stableFCs_allresults.txt' '/home/mike/timeseries_outputs/5D_2016_01_28/' parameter_file_path='/home/mike/DSGRN_outputs/new/5D_2016_01_28/'
+#example: ./runmany2b.sh '/home/mike/DSGRN-master/networks/5D_2016_01_28.txt' '/home/mike/conley3_outputs/5D_2016_01_28_stableFCs_allresults.txt' '/home/mike/timeseries_outputs/5D_2016_01_28/' '/home/mike/DSGRN_outputs/new/5D_2016_01_28/'
 
 #SUMMARY OF SCRIPT: This script uses: parse_conley3_3.py, make_pnMG.py, ToMathematicaTest, hillmodel.py, hillmodelscripts3.py, and compare.py
 #Given a network and one of its morse graphs, for
