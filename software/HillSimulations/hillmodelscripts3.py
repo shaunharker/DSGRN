e279bc19f064b41e9dc934c6fcdef7d10e69d81c
Mike Lan <mikelan17@gmail.com>
2016-02-27 11:51:49 -0500

    change hillmodelscripts

--- software/HillSimulations/hillmodelscripts3.py
+++ software/HillSimulations/hillmodelscripts3.py
@@ -26,66 +26,20 @@ reload (hm)
 import pdb
 import os
 
-#BEFORE RUNNING: CHANGE PATH NAMES OR DELETE PREV VERSIONS OF EXTREMA.TXT, FC_EXTREMA.TXT AND FC_EQNS.TXT BECAUSE THESE USE FILE(PATH,'a')
+#python hillmodelscripts3.py '923669' '/home/mike/DSGRN-master/networks/5D_2016_01_28.txt' '/home/mike/timeseries_outputs/5D_2016_01_28/' '/home/mike/DSGRN_outputs/new/5D_2016_01_28/923669.txt'
 
-def main(networkname,pnMG):
-    pnMG_dict = {}
-    parameterlist =[]
-    f=open(pnMG,'r')
-    for line in f:
-        splitted = line.split() 
-        pnMG_dict[splitted[1]] = splitted[0]
-        parameterlist.append(splitted[1])
-    counter = 0
-    for p in parameterlist:
-        counter += 1
-        HMS(p,networkname,pnMG_dict[p])
-        if counter % 500 == 0:
-            print(counter)
-
-def HMS(parameterstring,networkname,morsegraph):
-    # specify the full path to your file, unless it is in the same directory
-    networkfile = '/home/mike/DSGRN-master/networks/'+networkname+'.txt'
-
+def main(parameterstring,networkfile,savein_location,parameters_file):
     # choose a Hill exponent
     hillexp = 10
 
     #specifies the folder you'll save the graphs in, along with their file names
     
     #path of where output is saved
-    savein_ts0 = '/home/mike/timeseries_outputs/'+networkname
-    savein_ts = '/home/mike/timeseries_outputs/'+networkname+'/'+networkname+'_extrema_n10_3.txt'
-    savein_ts_2 = '/home/mike/timeseries_outputs/'+networkname+'/'+networkname+'_FC_extrema_n10_3.txt'
-    
-    savein_eqns0 = '/home/mike/eqn_FC_outputs/'+networkname
-    savein_eqns = '/home/mike/eqn_FC_outputs/'+networkname+'/'+networkname+'_FC_eqns_n10_2.txt'
+    savein = savein_location+'TimeSeries.txt'
 
-    #alt = 'Seagate Expansion Drive/z ubuntu'
-    alt = 'Cruzer'
-    savein0 = '/media/mike/'+alt+'/HillModel_outputs_n10/'+networkname+'/MG'+morsegraph
-    savein =  '/media/mike/'+alt+'/HillModel_outputs_n10/'+networkname+'/MG'+morsegraph+'/'+networkname+'_pn'+parameterstring+'.png'
-    savein0_2 = '/media/mike/'+alt+'/HillModel_FC_outputs_n10/'+networkname+'/MG'+morsegraph
-    savein_2 = '/media/mike/'+alt+'/HillModel_FC_outputs_n10/'+networkname+'/MG'+morsegraph+'/'+networkname+'_pn'+parameterstring+'.png'
-    savein0_3 = '/media/mike/'+alt+'/HillModel_XC_outputs_n10/'+networkname+'/MG'+morsegraph
-    savein_3 = '/media/mike/'+alt+'/HillModel_XC_outputs_n10/'+networkname+'/MG'+morsegraph+'/'+networkname+'_pn'+parameterstring+'.png'
-
-
-    if not os.path.exists(savein0):
-        os.makedirs(savein0) #if a folder AND its child folder don't exist, this creates both of them
-    if not os.path.exists(savein0_2):
-        os.makedirs(savein0_2)
-    if not os.path.exists(savein0_3):
-        os.makedirs(savein0_3)
-    if not os.path.exists(savein_ts0):
-        os.makedirs(savein_ts0)
-    if not os.path.exists(savein_eqns0):
-        os.makedirs(savein_eqns0)
-
-    #the following creates the sample file of parameters (samplefile2) using DSGRN outputs (raw_samples)
-    raw_samples = '/home/mike/DSGRN_outputs/new/'+networkname+'/'+parameterstring+'.txt'
-    
-    samples = '/home/mike/DSGRN_outputs/samples.txt'
-    f=open(raw_samples,'r')
+    #the following creates the sample file of parameters (samplefile2) using DSGRN outputs (parameters_file)
+    samples = os.getcwd() + '/samples.txt'
+    f=open(parameters_file,'r')
     for line in f:
         if '->' in line:
             line = line.replace("{{","")
@@ -94,20 +48,11 @@ def HMS(parameterstring,networkname,morsegraph):
             file_ = open(samples, 'w')
             file_.write(line)
             file_.close()
-    
-
-    samplefile2 = '/home/mike/DSGRN_outputs/samples.txt'
 
     # make an instance of class hillmodel
-    Example = hm.hillmodel(networkfile,samplefile2,hillexp)
+    HM_Instance = hm.hillmodel(networkfile,samples,hillexp)
 
-    dim = ''
-    for i in networkname:
-        if i == 'D':
-            break
-        else:
-            dim += i
-    dim=int(dim)
+    dim = HM_Instance.dim()
 
     # choose initial conditions and time period
     y0 = [1.0 for x in range(dim)] # range(W) for W variables in this network
@@ -116,7 +61,7 @@ def HMS(parameterstring,networkname,morsegraph):
     dt = 0.01 # give me a new data point every hundredth of a time unit
 
     # run the Hill model
-    times, timeseries = Example.simulateHillModel(savein_ts,savein_ts_2,savein_eqns,parameterstring,y0,t0,t1,dt) 
+    times, timeseries = HM_Instance.simulateHillModel(savein,parameterstring,y0,t0,t1,dt) 
 
     # choose plotting options
     plotoptions={'linewidth':2}
@@ -126,7 +71,7 @@ def HMS(parameterstring,networkname,morsegraph):
     figuresize = (15,10)
 
     # plot the results 
-    Example.plotResults(savein,savein_2,savein_3,times,timeseries,plotoptions,legendoptions,figuresize)
+    #Example.plotResults(savein,savein_2,savein_3,times,timeseries,plotoptions,legendoptions,figuresize)
 
 if __name__ == '__main__':
-    main(sys.argv[1],sys.argv[2])
+    main(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
