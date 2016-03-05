33989bc56bd32410d782cd13336e65c3f12c7787
Shaun Harker <sharker81@gmail.com>
2016-03-05 02:38:29 -0500

    hillmodel refactor debugging appears complete

--- software/HillSimulations/hillmodel.py
+++ software/HillSimulations/hillmodel.py
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+import re
 import numpy as np
 from scipy.integrate import ode
 import matplotlib.pyplot as plt
@@ -33,7 +34,7 @@ matplotlib.rc('font', **font)
 
 class hillmodel(object):
   '''
-  This class takes a network file, a parameter sample file, and a Hill
+  This class takes a network file, a parameter, and a Hill
   exponent and builds a Hill function model. The class has two public
   methods:
   1) time,timeseries = hillmodel.simulateHillModel(initialconditions,initialtime,finaltime,timestep)
@@ -41,13 +42,32 @@ class hillmodel(object):
   The first method generates a time series for a given set of initial conditions,
   and the second method plots the results. 
   '''
-  def __init__(self,networkfile,samplefile,hillexp):
+  def __init__(self,networkfile,parameter_file_or_dict,hillexp):
     '''
     Construct the Hill model for a given network and parameter sample.
+    Inputs:
+       networkfile -- filename of a network specification file
+       parameter_file_or_dict -- either (a) parameter specification filename
+                                     or (b) the dictionary object describing the parameter choice
+       hillexp -- Hill function exponent to use in the model
+    Note:
+      The format of the parameter specification file is that it contains in JSON format 
+      for a dictionary object giving a key-value mapping from parameters to numbers, e.g.
+      { "L[X, Y]" : 2.34848, "U[X, Y]" : 1.23888, ... }
     '''
-    eqnstr,self.varnames = self._parseEqns(networkfile)
-    parameternames,samples = self._parseSamples(self.varnames,samplefile)
-    self.eqns=self._makeHillEqns(eqnstr,parameternames,samples,hillexp)
+    eqnstr, self.varnames, self.varindex = self._parseEqns(networkfile)
+    if isinstance(parameter_file_or_dict, dict):
+      parameter = self._parseParameter(parameter_file_or_dict)
+    else:
+      parameter = self._parseSamples(parameter_file_or_dict)
+    self.eqns=self._makeHillEqns(eqnstr,parameter,hillexp)
+    self.d=len(eqnstr)
+
+  def dim(self):
+    """
+    Return number of variables in model
+    """
+    return self.d
 
   def simulateHillModel(self,initialconditions,initialtime,finaltime,timestep):
     '''
@@ -101,17 +121,18 @@ class hillmodel(object):
     Parse a network specification file to obtain data structures representing ODEs
       Input: "fname" is a path to a network specification file.
       Output: The function outputs 
-                eqnstr, varnames   
+                eqnstr, varnames, varindex 
               where
                 eqnstr   is a list of strings representing the ODE in a p-n formatting (see below)
-                varnames is a dictionary with keys being the variable names and values being an internal indexing
+                varnames is a list of variable names, the order of which gives an internal indexing
+                varindex is a dictionary with keys being the variable names and values being an internal indexing
 
       Note: "p-n formatting" of a network node's input formula replaces the variables occuring in 
             the string instead with the variable indices and suffixes them with either "n" or "p" 
             depending on whether they are negated. Multiplication in this formatting is always explicit 
             (never mere juxtaposition). It is easiest to describe by example:  
                       (~X + Y)(Z)  becomes ((0n)+(1p))*((2p)) when 
-                      varnames["X"] == 0, varnames["Y"] == 1, and varnames["Z"] == 2
+                      varindex["X"] == 0, varindex["Y"] == 1, and varindex["Z"] == 2
       Notes on Network specification file:
           A network spec file contains on each line 
             <varname> : <input-formula> [: E]
@@ -127,56 +148,55 @@ class hillmodel(object):
     """
     file = open(fname,'r')
     eqns=[]
-    variableToIndex = {}
+    varnames = []
+    varindex = {}
     for line in file:
-      # Parse line
       parsed = line.split(':')
       varname = parsed[0].strip() # e.g. "X"
       formula = parsed[1].strip() # e.g. "(~X + Y)U Z"
       # Disregard network spec comment lines
-      continue if varname[0] == '.' or varname[0] == '@' 
-      # Add entry in lookup table from variable name to variable index
-      varnames[varname]=str(len(varnames))
-      # Add entry in list of network node input formulas
+      if varname[0] == '.' or varname[0] == '@': continue
+      varnames.append(varname)
+      varindex[varname]=str(len(varindex))
       eqns.append(formula)
     file.close()
     eqnstr=[]
     for e in eqns:
       # Replace occurences of variables with variable indices followed by p if occurring with ~ prefix and followed by n otherwise
       # Example: "(~X + Y)U Z" --> "((0n) + (1p))(2p) (3p)"
-      e = re.sub('([ ()+*]*)(~?) *([^ ~()+*]+)([ ()+*]*)', lambda x: x.group(1) + "(" + varnames[x.group(3)] + ("n" if (x.group(2) == '~') else "p") + ')' + x.group(4), e)
+      e = re.sub('([ ()+*]*)(~?) *([^ ~()+*]+)([ ()+*]*)', lambda x: x.group(1) + "(" + varindex[x.group(3)] + ("n" if (x.group(2) == '~') else "p") + ')' + x.group(4), e)
       # Remove spaces and make multiplications explicit
       # Example: "((0n) + (1p))(2p) (3p)" --> "((0n)+(1p))*(2p)*(3p)"
       e = e.replace(' ','').replace(')(',')*(')
       # Add parsed equation to eqnstr output list
       eqnstr.append(e)
-    return eqnstr,varnames
+    print eqnstr
+    print varnames
+    print varindex
+    return eqnstr,varnames,varindex
       
-  def _parseParameter(self,varnames,parameter):
+  def _parseParameter(self,parameter):
     """
     Converts a parameter for internal use by replacing variable names with indices in the lookup table
-    Inputs:  "varnames" is a dictionary with keys being the variable names and values being an internal indexing
-                e.g. { "X" : "0", "Y" : "1", ... }
-             "parameter" is a key-value table from parameter names to numeric values, e.g.
+    Inputs:  "parameter" is a key-value table from parameter names to numeric values, e.g.
                 e.g. { "L[X, Y]" : 2.34848, "U[X, Y]" : 1.23888, ... }
     Outputs: the return value is obtained from rewriting parameter names using internal variable indexing
                 e.g. { "L[0,1]" : 2.34848, "U[0,1]" : 1.23888, ... }
     """
     replace = lambda key : re.sub(' *([LUT])\[ *([^ ]*) *, *([^ ]*) *\] *', 
-              lambda match : match.group(1) + '[' + varnames[match.group(2)] + ',' + varnames[match.group(3)] + ']')
+              lambda match : match.group(1) + '[' + self.varindex[match.group(2)] + ',' + self.varindex[match.group(3)] + ']', key)
     return { replace(key) : value for key, value in parameter.items() }
            
-  def _parseSamples(self,varnames,fname='samples.txt'):
+  def _parseSamples(self,fname='samples.txt'):
     """
     Read the samples and return the names of parameters along with their values.
-    Inputs:  varnames is a dictionary with keys being the variable names and values being an internal indexing
-             fname    is the name of a file containing parameter data
+    Inputs:  fname    is the name of a file containing parameter data
     Output:  parameter is a key-value table from parameter names (using variable indexing) to numeric values, e.g.
                 e.g. { "L[0,1]" : 2.34848, "U[0,1]" : 1.23888, ... }
     """
     with open(fname) as parameter_file:    
       named_parameter = json.load(parameter_file)
-    return self._parseParameter(varnames,named_parameter)
+    return self._parseParameter(named_parameter)
 
   def _makeHillStrs(self,U,L,T,n,J):
     """
@@ -184,7 +204,7 @@ class hillmodel(object):
        neghill = "(U-L)*(T**n)/(X[J]**n+T**n) + L"
        poshill = "(U-L)*(X[J]**n)/(X[J]**n+T**n) + L"
        with the appropriate values for U, L, T, n, and J substituted
-    """
+    """    
     scalar = "("+U+"-"+L+")"
     Xn = "X["+J+"]**"+n
     Tn = T+"**"+n
@@ -219,12 +239,13 @@ class hillmodel(object):
         J = match.group(1)  # integer which indexes input variable
         regulation = match.group(2) # either "n" or "p"
         pair = "["+J+","+K+"]"
-        U = parameter["U" + pair]
-        L = parameter["L" + pair]
-        T = parameter["T" + pair]
-        neghill, poshill = self._makeHillStrs(U,L,T,n,J)
-        return (poshill if match.group(2) == 'p' else neghill)
+        U = str(parameter["U" + pair])
+        L = str(parameter["L" + pair])
+        T = str(parameter["T" + pair])
+        neghill, poshill = self._makeHillStrs(U,L,T,str(n),J)
+        return (poshill if regulation == 'p' else neghill)
       # Include the formula into the expression
       expression += (',' if len(expression) > 1 else '') + "-X["+K+"]+" + re.sub('([0-9]*)([np])', replaceWithHillFunction, e)
     expression += ']'
+    print expression
     return eval('lambda X :' + expression)
