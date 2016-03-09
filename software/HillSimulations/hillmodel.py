26aaa67de0d8eeccf3426c0c9d8f620b8fab91d7
Shaun Harker <sharker81@gmail.com>
2016-03-09 05:07:58 -0500

    fixed a bug in hillmodel.py

--- software/HillSimulations/hillmodel.py
+++ software/HillSimulations/hillmodel.py
@@ -97,7 +97,7 @@ class hillmodel(object):
     r = ode(RHS).set_integrator('vode', method='bdf')
     r.set_initial_value(initialconditions,initialtime).set_f_params(self.eqns)
     times,timeseries = integrate(r,initialconditions,initialtime,finaltime,timestep)
-    return times,timeseries,varnames
+    return times,timeseries,self.varnames
 
   def plotResults(self,times,timeseries,plotoptions={},legendoptions={},figuresize=()):
     '''
