9a084ad9df0b9fb9517b3537ca5436c55a45bb32
Shaun Harker <shaun.harker@geomdata.com>
2018-12-06 13:54:43 -0500

    lowercase true

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 rm -rf build
 rm -rf dist
 git submodule update --init --recursive
-pip uninstall -y DSGRN &> /dev/null || True
+pip uninstall -y DSGRN &> /dev/null || true
 pip install . --upgrade --no-deps --force-reinstall --no-cache-dir $@
